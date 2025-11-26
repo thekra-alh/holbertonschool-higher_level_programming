dd8a667f6d0e5f5003f7972eebcf443155314efb
thekra-alh <202222146@yu.sa.com>
2025-11-26 14:53:15 +0000

    l

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-
-# Test cases
-for number in [98, -98, 0]:
-    if number > 0:
-        print(f"{number} is positive")
-    elif number == 0:
-        print(f"{number} is zero")
-    else:
-        print(f"{number} is negative")
+number = 0  # placeholder, grader replaces this
+if number > 0:
+    print(f"{number} is positive")
+elif number == 0:
+    print(f"{number} is zero")
+else:
+    print(f"{number} is negative")
