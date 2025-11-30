50ea6732a16fc9442c5e4257e4a86294b5506e2c
thekra-alh <202222146@yu.sa.com>
2025-11-30 15:46:33 +0000

    s

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,10 @@
-#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    for i in range(x):
-        try:
-            print("{}".format(my_list[i]), end="")
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
             count += 1
-        except IndexError:
-            break
+    except IndexError:
+        pass
     print()
     return count
