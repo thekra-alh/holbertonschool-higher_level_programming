9d4091d5d23a1746632c289b11cada48c6d255fd
thekra-alh <202222146@yu.sa.com>
2025-12-01 13:16:36 +0000

    l

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,4 +1,15 @@
+#!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
+    """Print x elements of a list.
+    
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): The number of elements of my_list to print.
+    
+    Returns:
+        The real number of elements printed.
+    """
     count = 0
     try:
         for i in range(x):
@@ -6,6 +17,5 @@ def safe_print_list(my_list=[], x=0):
             count += 1
     except IndexError:
         pass
-    finally:
-        print()  # Print new line after all elements
+    print()
     return count
