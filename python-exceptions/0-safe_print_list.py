092a16c31ebf14a55c964c9632fb3afec9698bb0
thekra-alh <202222146@yu.sa.com>
2025-12-01 14:39:00 +0000

    l

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    """Print x elements of a list.
-    
-    Args:
-        my_list (list): The list to print elements from.
-        x (int): The number of elements of my_list to print.
-    
-    Returns:
-        The real number of elements printed.
-    """
     count = 0
     try:
-        for i in range(x):
+        for i iin range(x):
             print(my_list[i], end="")
             count += 1
     except IndexError:
--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
-
 def safe_print_division(a, b):
-    """Divide two integers and print the result.
-    
-    Args:
-        a (int): The numerator.
-        b (int): The denominator.
-    
-    Returns:
-        The result of division, or None if division fails.
-    """
     result = None
     try:
         result = a / b
-    except ZeroDivisionError:
+    except Exception:
         result = None
     finally:
         print("Inside result: {}".format(result))
