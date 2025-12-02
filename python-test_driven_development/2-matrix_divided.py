8e8ac7cc5000854cd97aabd61204f919209e232c
thekra-alh <202222146@yu.sa.com>
2025-12-02 10:55:35 +0000

    l

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -34,4 +34,7 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
+    if div == float('inf') or div == float('-inf'):
+    return [[0.0 for _ in row] for row in matrix]
+
     return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
