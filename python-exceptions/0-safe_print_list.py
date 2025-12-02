03c73be5ab2bff97e033bef33c42e7514a960993
thekra-alh <202222146@yu.sa.com>
2025-12-02 09:23:02 +0000

    l

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
-
 def safe_print_list(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end="")
+            print(my_list[i], end="")
             count += 1
-        except IndexError:
+        except Exception:
             break
     print()
     return count
