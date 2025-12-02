5b8b7e693824926b502b53ad04fb757f494694b0
thekra-alh <202222146@yu.sa.com>
2025-12-02 09:19:02 +0000

    k

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,13 @@
+#!/usr/bin/python3
 
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (TypeError, ValueError):
+            # Not an integer → skip silently
+            continue
+    print()
+    return count
