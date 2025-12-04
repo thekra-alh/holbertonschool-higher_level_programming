8514484b7c02f0a3f1074146791492cfacd31fa0
thekra-alh <202222146@yu.sa.com>
2025-12-04 13:36:30 +0000

    s

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
+"""
+Module for adding two integers.
+This module provides a function to add two numbers.
+"""
 
 
 def add_integer(a, b=98):
     """
-    adds an integer
-    unit tests located in tests/0-add_integer.txt
-    checks for type errors
+    Adds two integers or floats.
+    Returns the sum as an integer.
     """
-    if not isinstance(a, int):
-        if isinstance(a, float):
-            a = int(a)
-        else:
-            raise TypeError("a must be an integer")
-    if not isinstance(b, int):
-        if isinstance(b, float):
-            b = int(b)
-        else:
-            raise TypeError("b must be an integer")
-    return a + b
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
