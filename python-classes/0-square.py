075b1a5fce2759de5f811b699050a49d86cbcea3
thekra-alh <202222146@yu.sa.com>
2025-12-07 07:59:34 +0000

    l

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Module that defines an empty Square class."""
+
 class Square:
-    """An empty class that defines a square."""
+    """Empty class that defines a square."""
     pass
