af7dca53262ca7058255e81585e22d0a40cbadd1
thekra-alh <202222146@yu.sa.com>
2025-12-07 08:10:11 +0000

    l

---
+++ python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Square class definition."""
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size=0):
+        """Initialize a square with size."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieve the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square with validation."""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Print the square with the character #."""
+        if self.__size == 0:
+            print("")
+            return
+
+        for _ in range(self.__size):
+            print("#" * self.__size)
