55d7677923e16851756db6276a2dc2f787b2d0e2
thekra-alh <202222146@yu.sa.com>
2026-01-13 19:05:55 +0000

    s

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,7 @@
+-- 15. Number by score
+-- Lists the number of records with the same score in the table second_table of the database hbtn_0c_0
+
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC;
