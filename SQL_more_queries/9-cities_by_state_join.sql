b807075ca19535becf12bde7d5db20842973dc6d
thekra-alh <202222146@yu.sa.com>
2026-01-15 14:52:15 +0000

    s

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- 9. Cities by States
+-- Lists all cities contained in the database.
+
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
