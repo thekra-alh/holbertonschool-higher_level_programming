0599f24ce9ee06b2d91db4b16cea09d3c0f5c25b
thekra-alh <202222146@yu.sa.com>
2026-01-15 14:47:46 +0000

    s

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,11 @@
+-- 8. Cities of California
+-- Lists all cities that can be found in the database.
+
+SELECT id, name
+FROM cities
+WHERE state_id = (
+    SELECT id
+    FROM states
+    WHERE name = 'California'
+    )
+ORDER BY id ASC;
