35b5b1bd0e4a7ff37d3f56f00dfb84f08bc160f5
thekra-alh <202222146@yu.sa.com>
2026-01-15 14:40:42 +0000

    s

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,9 @@
+-- 2. Read user
+--  Creates a database and a user
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
